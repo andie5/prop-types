c6cf2e10c7ae64574f03f7422aa3650356aa3fd5
andie5 <andieasa@googlemail.com>
2019-11-16 20:05:01 +0000

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import PropTypes from "prop-types";
+
+const AddressLabel = (personAddress) => {
+  const { name, addressLine1, addressLine2 } = personAddress;
+  return (
+    <>
+      <div>Name: {name}</div>
+      <div>Address 1: {addressLine1}</div>
+      <div>Address 2: {addressLine2}</div>
+    </>
+  );
+}
+
+const personAddress = {
+  name: "John Doe",
+  addressLine1: "123 Fake St. Boston, ",
+  addressLine2: "MA 02118",
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AddressLabel person={personAddress} />
   );
 }
+
+
+AddressLabel.PropTypes = {
+  personAddress: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    addressLine1: PropTypes.string.isRequired,
+    addressLine2: PropTypes.string.isRequired
+  })
+}
+
+
 
 export default App;
