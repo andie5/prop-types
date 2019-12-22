580b61262f5e68a015b25a6cd9c5980472fb7fee
andie5 <andieasa@googlemail.com>
2019-12-22 23:47:28 +0000

    added css class for address label

--- src/App.js
+++ src/App.js
@@ -1,38 +1,64 @@
-import React from 'react';
+import React from "react";
 import PropTypes from "prop-types";
+import "./index.css";
 
-const AddressLabel = (personAddress) => {
-  const { name, addressLine1, addressLine2 } = personAddress;
+const AddressLabel = ({ person }) => {
+  const { name, addressLine1, addressLine2 } = person;
+
   return (
     <>
-      <div>Name: {name}</div>
-      <div>Address 1: {addressLine1}</div>
-      <div>Address 2: {addressLine2}</div>
+      <div>{name}</div>
+      <div>{addressLine1}</div>
+      <div>{addressLine2}</div>
     </>
   );
-}
+};
 
-const personAddress = {
-  name: "John Doe",
+const fromAddress = {
+  name: "Mr Sender",
   addressLine1: "123 Fake St. Boston, ",
-  addressLine2: "MA 02118",
-}
+  addressLine2: "MA 02118"
+};
+
+const toAddress = {
+  name: "Mr Receiver",
+  addressLine1: "123 Fake St. Boston, ",
+  addressLine2: "CA 12345"
+};
 
 function App() {
+  return <Envelope toPerson={toAddress} fromPerson={fromAddress} />;
+}
+
+function Envelope({ toPerson, fromPerson }) {
   return (
-    <AddressLabel person={personAddress} />
+    <div className={"envelope"}>
+      <AddressLabel className="fromLabel" person={fromPerson} />
+      <AddressLabel className="toLabel" person={toPerson} />
+      <Stamp />
+    </div>
   );
 }
 
+Envelope.propTypes = {
+  toPerson: PropTypes.object.isRequired,
+  fromPerson: PropTypes.object.isRequired
+};
 
-AddressLabel.PropTypes = {
+function Stamp() {
+  return (
+    <div className="stamp">
+      <span className="text">Stamp</span>
+    </div>
+  );
+}
+
+AddressLabel.propTypes = {
   personAddress: PropTypes.shape({
     name: PropTypes.string.isRequired,
     addressLine1: PropTypes.string.isRequired,
     addressLine2: PropTypes.string.isRequired
   })
-}
-
-
+};
 
 export default App;
