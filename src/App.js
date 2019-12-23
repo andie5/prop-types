5d68bbd4eb9869bebab37766c18248eb1c1cfcdb
andie5 <andieasa@googlemail.com>
2019-12-23 23:01:37 +0000

    added prop-types for email

--- src/App.js
+++ src/App.js
@@ -3,48 +3,35 @@ import PropTypes from "prop-types";
 import "./index.css";
 
 function App() {
+  return <Email email={emailMessage} />;
+}
+
+function Email({ email }) {
+  const { sender, subject, date, message } = email;
   return (
-    <Poster
-      image={"https://rawgit.com/gorangajic/react-icons/master/react-icons.svg"}
-      title={"REACT"}
-      text={"The best thing since jQuery, probably"}
-    />
+    <>
+      <div>{sender}</div>
+      <div>{subject}</div>
+      <div>{date}</div>
+      <div>{message}</div>
+    </>
   );
 }
 
-function Poster({ image, title, text }) {
-  console.log("image: ", image);
-  console.log("image var: ", `${image}`);
-  return (
-    <div className={"poster"}>
-      <div>
-        <img
-          src={image}
-          alt={"react-img"}
-          style={{ width: "200px", alignSelf: "center", paddingLeft: "249px" }}
-        />
-      </div>
-      <div
-        style={{
-          textAlign: "center",
-          fontFamily: "serif",
-          fontSize: "2.5em",
-          color: "blue"
-        }}
-      >
-        {title}
-      </div>
-      <div style={{ textAlign: "center", color: "#fff", fontSize: "1.5em" }}>
-        {text}
-      </div>
-    </div>
-  );
-}
+var emailMessage = {
+  sender: "Mr Sender",
+  subject: "Test subject",
+  date: "01/01/1999",
+  message: "This is the email message."
+};
 
-Poster.propTypes = {
-  image: PropTypes.string.isRequired,
-  text: PropTypes.string.isRequired,
-  title: PropTypes.string.isRequired
+Email.propTypes = {
+  email: PropTypes.shape({
+    sender: PropTypes.string,
+    subject: PropTypes.string,
+    date: PropTypes.string,
+    message: PropTypes.string
+  }).isRequired
 };
 
 export default App;
